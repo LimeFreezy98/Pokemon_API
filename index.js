49e980066b5ff40d1d20d1a90ecc73a287f94c71
LimeFreezy98 <89162212+LimeFreezy98@users.noreply.github.com>
2025-09-26 09:08:05 -0600

    adding function to able see image and names when clicked each pokemon names

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 async function fetchPokemon() {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20");
     const data = await response.json();
-    return data.results;
+    return data.results; // list of pokemon { name, url }
+}
+
+async function fetchPokemonDetails(pokemonName) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+    return await response.json();
 }
 
 async function displayPokemon() {
@@ -10,11 +15,21 @@ async function displayPokemon() {
 
     pokemons.forEach(pokemon => {
         const li = document.createElement("li");
-        li.textContent = pokemon.name
+        li.textContent = pokemon.name;
+
+        // Add click event
+        li.addEventListener("click", async () => {
+            const details = await fetchPokemonDetails(pokemon.name);
+            
+            const detailsDiv = document.getElementById("pokemon-details");
+            detailsDiv.innerHTML = `
+                <h3>${details.name.toUpperCase()}</h3>
+                <img src="${details.sprites.front_default}" alt="${details.name}">
+            `;
+        });
+
         pokemonList.appendChild(li);
-    })
+    });
 }
-
-
 
 displayPokemon();
