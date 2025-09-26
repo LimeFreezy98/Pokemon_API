6330bb338bcf39e524046b98ddf62d72115fba6e
LimeFreezy98 <89162212+LimeFreezy98@users.noreply.github.com>
2025-09-26 08:36:49 -0600

    adding javascript api function to show pokemon name using fetch function and fix missing <html> on index.html

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ async function fetchPokemon() {
     const data = await response.json();
     return data.results;
 }
+
+async function displayPokemon() {
+    const pokemonList = document.getElementById("pokemon-list");
+    const pokemons = await fetchPokemon();
+
+    pokemons.forEach(pokemon => {
+        const li = document.createElement("li");
+        li.textContent = pokemon.name
+        pokemonList.appendChild(li);
+    })
+}
+
+
+
+displayPokemon();
